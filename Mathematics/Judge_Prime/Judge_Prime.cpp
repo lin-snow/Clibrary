20dc42ae67b9b8e63eef30ea4de23babf4ebcb41
L1nSn0w <l1nsn0w@qq.com>
2024-08-29 13:27:50 +0800

    chore: Update launch.json and settings.json for C/C++ debugging configuration add Judge_Prime.cpp

---
+++ Mathematics/Judge_Prime/Judge_Prime.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    system("cls"); // 清屏
+
+    int isPrime(int n); // 函数原型
+
+    int x = scanf("%d", &x); // 输入待判断的数
+
+    if (isPrime(x)) {
+        printf("%d is Prime!\n", x);
+    } else {
+        printf("%d isn't Prime!\n", x);
+    }
+
+    system("pause");
+    return 0;
+}
+
+int isPrime(int n) {
+    // 判断是否小于等于1
+    if ( n <= 1 ) return 0;
+
+    // 判断是否为偶数
+    if ( n % 2 == 0 ) return 0;
+
+    // 从3开始，逐个检查输入的数能否3 到 n之间的奇数整除
+    for(int i = 3; i <=  sqrt(n); i += 2) {
+        if (n % i == 0) return 0; 
+    }
+
+    return 1;
+}
